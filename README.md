8e0ea3746ea41b6ae645d895731cbca9ea4b2ce0
lsaikiran <55637934+lsaikiran@users.noreply.github.com>
2024-04-08 21:47:44 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# terminal_simulation
+# Container Terminal Simulation
+
+**Description:**
+This Python script simulates the operations of a container terminal, where vessels arrive for unloading containers, which are then transferred to trucks for transportation to the yard. The simulation models the concurrent processes of vessel berthing, container unloading using cranes, and truck movement within the terminal.
+
+**Features:**
+- Simulates the arrival of vessels at the terminal according to an exponential distribution.
+- Utilizes SimPy, a process-based discrete-event simulation framework.
+- Tracks the movement of containers from vessels to trucks, considering resource constraints such as berth availability, crane capacity, and truck availability.
+- Provides real-time logging of events during the simulation for monitoring and analysis.
+
+**Key Components:**
+1. **ContainerTerminal Class**: Represents the terminal entity, managing resources such as berths, cranes, and trucks. It handles the processes of vessel berthing, container unloading, and truck movement.
+   
+2. **vessel_generator Function**: Generates vessels at random intervals based on an exponential distribution, simulating their arrival at the terminal.
+
+3. **run_simulation Function**: Initializes the simulation environment, prompts the user to input the simulation time, and runs the simulation until the specified time.
+
+**Dependencies:**
+- Python 3.x
+- SimPy library
+
+**How to Use:**
+1. Ensure Python and SimPy library are installed on your system.
+2. Run the script using a Python interpreter.
+3. Input the desired simulation time in minutes when prompted.
+4. Monitor the simulation output in the console, which provides real-time updates on events occurring at the container terminal.
+
+**Contributors:**
+- This simulation script was developed by Sai Kiran for educational and demonstrative purposes.
+
+**Disclaimer:**
+This simulation is a simplified model for educational use and may not accurately reflect real-world container terminal operations. Adjustments and enhancements may be necessary for specific use cases or detailed analysis.
--- terminal_simulation.py
+++ terminal_simulation.py
@@ -12,6 +12,11 @@ ARRIVAL_RATE = 1 / 300  # 5 hours in min
 
 
 class ContainerTerminal:
+    """
+    Represents the terminal entity, managing resources such as berths, cranes, and trucks. 
+    It handles the processes of vessel berthing, container unloading, and truck movement.
+    """
+
     def __init__(self, env):
         self.env = env
         self.berths = simpy.Resource(env, capacity=NUM_OF_BERTHS)
@@ -20,19 +25,33 @@ class ContainerTerminal:
         self.berth_offset = 0
         self.crane_offset = 0
 
+    def log_message(self, message):
+        """
+            Log message along with env.now
+        """
+        print(f'{self.env.now}: {message}')
+
     def berth_vessel(self, vessel):
+        """
+          Requests for an empty berth and holds 
+          it until vessel is discharged
+        """
         with self.berths.request() as berth:
             yield berth  # waiting for berth
             berth_id = (self.berth_offset % NUM_OF_BERTHS) + 1
             self.berth_offset += 1
 
-            print(f"{self.env.now}: Vessel {vessel} berthed at berth {berth_id}")
+            self.log_message(f'Vessel {vessel} berthed at berth {berth_id}')
             yield self.env.process(self.discharge_vessel(vessel))
-            print(f"{self.env.now}: Vessel {vessel} unloaded")
+            self.log_message(f'Vessel {vessel} unloaded')
 
-        print(f"{self.env.now}: Vessel {vessel} left the berth {berth_id}")
+        self.log_message(f'Vessel {vessel} left the berth {berth_id}')
 
     def discharge_vessel(self, vessel):
+        """
+            Requests for an empty crane and unloads 
+            containers while waiting for trucks
+        """
         with self.cranes.request() as crane:
             yield crane  # waiting for crane
             crane_id = (self.crane_offset % NUM_OF_CRANES) + 1
@@ -43,8 +62,8 @@ class ContainerTerminal:
                 # Check if any truck is available
                 if self.trucks.count < NUM_OF_TRUCKS:
                     containers_unloaded += 1
-                    print(
-                        f"{self.env.now}: Crane {crane_id} unloading container-{containers_unloaded} from vessel-{vessel}")
+                    self.log_message(
+                        f'Crane {crane_id} unloading container {containers_unloaded} from vessel {vessel}')
                     yield self.env.timeout(CONTAINER_MOVE_TIME)
                     self.env.process(self.move_truck_to_yard(
                         containers_unloaded, vessel))
@@ -54,31 +73,43 @@ class ContainerTerminal:
                     continue
 
     def move_truck_to_yard(self, container, vessel):
+        """
+            Moves the container from terminal to yard
+        """
         with self.trucks.request() as truck:
             yield truck
-            print(
-                f"{self.env.now}: Container-{container} from vessel-{vessel} is on its way to yard")
+            self.log_message(
+                f'Container {container} from vessel {vessel} is on its way to yard')
             yield self.env.timeout(TRUCK_MOVE_TIME)
-            print(
-                f"{self.env.now}: Truck carrying container-{container} from vessel-{vessel} is back at terminal")
+            self.log_message(
+                f'Truck carrying container {container} from vessel {vessel} is back at terminal')
 
 
 def vessel_generator(env, terminal):
+    """
+        Generates vessels at random intervals based on an exponential distribution,
+        simulating their arrival at the terminal.
+    """
     vessel_id = 0
     while True:
         yield env.timeout(int(random.expovariate(ARRIVAL_RATE)))
         vessel_id += 1
-        print(f"{env.now}: Vessel {vessel_id} has arrived and waiting to berth")
+        terminal.log_message(
+            f'Vessel {vessel_id} has arrived and waiting to berth')
         env.process(terminal.berth_vessel(vessel_id))
 
 
 def run_simulation():
-    simulation_time = int(input("Please enter simulation time(in min): "))
+    """
+    Initializes the simulation environment, prompts the user to input the simulation time,
+    and runs the simulation until the specified time.
+    """
+    simulation_time = int(input('Please enter simulation time(in min): '))
     env = simpy.Environment()
     terminal = ContainerTerminal(env)
     env.process(vessel_generator(env, terminal))
     env.run(until=simulation_time)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run_simulation()
